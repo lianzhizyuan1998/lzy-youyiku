43b3c00b9ce3a1b785c1cc40bdaeb0cf83103d39
lzy <1269065830@qq.com>
2020-09-29 22:00:25 +0800

    第五天的提交

--- php/index2.php
+++ php/index2.php
@@ -2,15 +2,18 @@
 header('Access-Control-Allow-Origin:*');
 header('Access-Control-Allow-Method:POST,GET');
 $arr = array(
-    array('sid'=>'1','url'=>'https://www.uniqlo.cn/hmall/test/u0000000018084/main/first/561/1.jpg','price'=>'299.00','title'=>'高腰修身牛仔九分裤(水洗产品) 429099','title1'=>'女装','type'=>'150/56A-175/90B',
+    array('sid'=>'1','url'=>'https://www.uniqlo.cn/hmall/test/u0000000017551/main/first/561/1.jpg','price'=>'149.00','title'=>'运动拉链连帽开衫(长袖 卫衣) 428322','title1'=>'女装','type'=>'150/56A-175/90B',
     'span'=>'',
-    'span1'=>'https://www.uniqlo.cn/cms/c5817f7946c8d912c70b6c6b7638421e.jpg'),
-    array('sid'=>'2 ','url'=>'https://www.uniqlo.cn/hmall/test/u0000000019203/main/first/561/1.jpg','price'=>'199.00','title'=>'(UT) PEANUTS连帽卫衣(长袖) 433684','title1'=>'女装','type'=>'XS-XXL','span1'=>'','span'=>'',),
-    array('sid'=>'3','url'=>'https://www.uniqlo.cn/hmall/test/u0000000018118/main/first/561/1.jpg','price'=>'299.00','title'=>'乔其纱荷叶边领印花衬衫(长袖)(优衣库云朵衫) 430818','title1'=>'女装','type'=>'XS-3XL','span1'=>'','span'=>'',),
-    array('sid'=>'4','url'=>'https://www.uniqlo.cn/hmall/test/u0000000017763/main/first/561/1.jpg','price'=>'249.00','title'=>'牛仔针织裙(水洗产品) 428343','title1'=>'女装','type'=>'XS-3XL','span1'=>'','span'=>'',),
-    array('sid'=>'5','url'=>'https://www.uniqlo.cn/hmall/test/u0000000017362/main/first/561/1.jpg','price'=>'299.00','title'=>'花式针织连衣裙(五分袖) 430761','title1'=>'女装','type'=>'XS-3XL','span'=>'https://www.uniqlo.cn/cms/df800e97fc36e995c486d41c9c7634d5.jpg',
-    'span1'=>'https://www.uniqlo.cn/cms/ee953823d4c1d280caa600e63a00fdf0.jpg'
-),
+    'span1'=>'https://www.uniqlo.cn/cms/c483b385090992775021ea8ec5de9d6a.jpg','startvalue'=>'初上市价格：¥199.00','hot'=>'(28)'),
+    array('sid'=>'2 ','url'=>'https://www.uniqlo.cn/hmall/test/u0000000018002/main/first/561/1.jpg','price'=>'99.00','title'=>'法兰绒衬衫(长袖) 430591','title1'=>'女装','type'=>'XS-XXL','span1'=>'https://www.uniqlo.cn/cms/c483b385090992775021ea8ec5de9d6a.jpg','span'=>'',
+    'startvalue'=>'初上市价格：¥149.00','hot'=>'(13)'),
+    array('sid'=>'3','url'=>'https://www.uniqlo.cn/hmall/test/u0000000019162/main/first/561/1.jpg','price'=>'499.00','title'=>'双面呢茧形大衣 429473','title1'=>'女装','type'=>'XS-XXL','span1'=>'https://www.uniqlo.cn/cms/c483b385090992775021ea8ec5de9d6a.jpg','span'=>'',
+    'startvalue'=>'初上市价格：¥599.00','hot'=>'(3)'),
+    array('sid'=>'4','url'=>'https://www.uniqlo.cn/hmall/test/u0000000018226/main/first/561/1.jpg','price'=>'129.00','title'=>'(UT) Furry Friends卫衣(长袖) 432392','title1'=>'女装','type'=>'XS-XXL','span1'=>'https://www.uniqlo.cn/cms/c483b385090992775021ea8ec5de9d6a.jpg','span'=>'',
+    'startvalue'=>'初上市价格：¥129.00','hot'=>'(3)'),
+    array('sid'=>'5','url'=>'https://www.uniqlo.cn/hmall/test/u0000000009993/main/first/561/1.jpg','price'=>'299.00','title'=>'HEATTECH圆领T恤(长袖)(温暖内衣) 418325','title1'=>'女装','type'=>'XS-3XL','span'=>'',
+    'span1'=>'https://www.uniqlo.cn/cms/c483b385090992775021ea8ec5de9d6a.jpg'
+,'startvalue'=>'初上市价格：¥79.00','hot'=>'(22)'),
 
 );
 
--- src/script/index_module.js
+++ src/script/index_module.js
@@ -1,10 +1,19 @@
 define([], function() {
     return {
-        // 第一部分渲染为热销排行
+
         init: function() {
+
+            // 懒加载
+            $(function() { //和拼接的元素放在一起。
+                $("img.lazy").lazyload({
+                    effect: "fadeIn" //图片显示方式
+                });
+            });
+
+            // 第一部分渲染为热销排行women
             let $tab1 = $('.tab1 .active-sceond')
             $.ajax({
-                    url: "http://192.168.13.54/lzy-youyiku/php/index2.php",
+                    url: "http://192.168.13.26/lzy-youyiku/php/index2.php",
                     dataType: 'json'
                 })
                 .done(function(data) {
@@ -18,21 +27,24 @@ define([], function() {
                                     <span><img src='${value.span}'></span>
                                     <span><img src='${value.span1}'></span>
                                     </div> 
-                                    <img src="${value.url}">
+                                    <img  src="${value.url}">
                                     <div class="tab-color"></div>
                                     <p>${value.title1}<span>${value.type}</span>
                                     <p>${value.title}</p>
-                                    <span>￥${value.price}</span>
-                                    <div class="star"><i></i><i></i><i></i><i></i><i></i></div>
+                                    <p class="startvalue">${value.startvalue}</p>
+                                    <span style="color:red;">￥${value.price}</span>
+                                    <div class="star"><i></i><i></i><i></i><i></i><i></i><span>${value.hot}<span></div>
                                     </a>
                                     </li>
                                     `;
                     });
                     $tab1.html($strhtml);
                 });
+
+            //第二部分渲染为热销排行men
             let $tab2 = $('.tab1 .active-third')
             $.ajax({
-                    url: "http://192.168.13.54/lzy-youyiku/php/index3.php",
+                    url: "http://192.168.13.26/lzy-youyiku/php/index3.php",
                     dataType: 'json'
                 })
                 .done(function(data) {
@@ -46,7 +58,7 @@ define([], function() {
                                     <span><img src='${value.span}'></span>
                                     <span><img src='${value.span1}'></span>
                                     </div> 
-                                    <img src="${value.url}">
+                                    <img class='i1' src="${value.url}">
                                     <div class="tab-color"></div>
                                     <p>${value.title1}<span>${value.type}</span>
                                     <p>${value.title}</p>
@@ -58,10 +70,11 @@ define([], function() {
                     });
                     $tab2.html($strhtmla);
                 });
-            //第二部分渲染
+
+            //第三部分渲染穿搭推荐-合作系列-ut部分-特辑部分
             let $a1 = $('.xuanran')
             $.ajax({
-                url: "http://192.168.13.54/lzy-youyiku/php/index1.php",
+                url: "http://192.168.13.26/lzy-youyiku/php/index1.php",
                 dataType: 'json'
             }).done(function(data) {
                 let $renderdata1 = data;
@@ -69,16 +82,22 @@ define([], function() {
                 $.each($renderdata1, function(index, value) {
                     $strhtml1 += `
                     <div class="xuanran1">
-                    <div class="xuanran1-title"><img src='${value.title}'></div>
-                    <div><a href="./blank-jump-page.html" target="_blank"><img src='${value.content}'></a></div>
-                    <div><a href="./blank-jump-page.html" target="_blank"><img src='${value.finally}'></a></div>
+                    <div class="xuanran1-title"><img class='lazy' data-original='${value.title}'></div>
+                    <div><a href="./blank-jump-page.html" target="_blank"><img class='lazy' data-original='${value.content}'></a></div>
+                    <div><a href="./blank-jump-page.html" target="_blank"><img class='lazy' data-original='${value.finally}'></a></div>
                     </div>
                     `
                 });
                 $a1.html($strhtml1);
-
+                //懒加载
+                $(function() { //和拼接的元素放在一起。
+                    $("img.lazy").lazyload({
+                        effect: "fadeIn" //图片显示方式
+                    });
+                });
             });
-            //tab切换
+
+            //tab切换 热门商品分类
             const $btns = $('.content-top ul li');
             const $divs = $('.content-list ul li');
             //按钮添加添加事件。
@@ -86,7 +105,8 @@ define([], function() {
                 $(this).addClass('active-tab').siblings('li').removeClass('active-tab');
                 $divs.eq($(this).index()).show().siblings('li').hide();
             });
-            //top
+
+            //top滑块效果 
             const $top = $('.topblock');
             $(window).on('scroll', function() {
                 let $srcolla = $(window).scrollTop();
@@ -101,6 +121,7 @@ define([], function() {
                 var speed = 600;
                 $('body,html').animate({ scrollTop: 0 }, speed);
             });
+
             //头部悬浮
             const $nav = $('.header ul')
             const $nav1 = $('.header ul .logof a')
@@ -110,18 +131,15 @@ define([], function() {
                     $nav.css('position', 'fixed');
                     $nav.css('background-color', 'white');
                     $nav.css('opacity', '0.9');
-                    $nav.css('left', '312px')
                     $nav1.css('display', 'block')
-
-
                 } else {
                     $nav.css('position', 'relative');
                     $nav.css('opacity', '1');
-                    $nav.css('left', '0')
                     $nav1.css('display', 'none')
                 }
             });
-            // tab切换2
+
+            // tab切换热销排行
             const $btns2 = $('.tab .tabtop ul li');
             const $divs2 = $('.tab1 ul ');
             //按钮添加添加事件。
@@ -129,70 +147,156 @@ define([], function() {
                 $(this).addClass('active-first').siblings('li').removeClass('active-first');
                 $divs2.eq($(this).index()).show().siblings('ul').hide();
             });
-            //轮播图1
-            var minNum = 0;
-            var maxNum = 5;
-            // 刚开始索引为0
-            var index = 0;
-            // 设置一个计时器为空
-            var counter = null;
-            var limWidth = 1170;
-            // 设置自动轮播函数
-            function autoMove() {
-                if (counter != null) {
-                    return;
-                }
-                counter = setInterval(autoPlay, 1000);
-            }
 
-            function autoPlay() {
-                cliMove('right');
-            }
-
-            function cliMove(direction) {
-                $('.img-list').stop(false, true);
-                if (direction == "right") {
-                    index++;
-                    if (index > 5) {
-                        $('.img-list').css({ left: '0' })
-                        index = 1;
+            // 标题轮播图 不带圆点
+            $(function() {
+                //轮播图1
+                var minNum = 0;
+                var maxNum = 5;
+                // 刚开始索引为0
+                var index = 0;
+                // 设置一个计时器为空
+                var counter = null;
+                var limWidth = 1170;
+                // 设置自动轮播函数
+                function autoMove() {
+                    if (counter != null) {
+                        return;
                     }
-                } else if (direction == "left") {
-                    index--;
-                    if (index < 0) {
-                        $('.img-list').css({
-                            left: maxNum * -limWidth
-                        })
-                        index = 4;
-                    }
+                    counter = setInterval(autoPlay, 1000);
                 }
-                $('.img-list').animate({
-                    left: index * -limWidth
-                });
-            }
-            $('.title').mouseenter(function() {
-                clearInterval(counter);
-                counter = null;
-            }).mouseleave(function() {
+
+                function autoPlay() {
+                    cliMove('right');
+                }
+
+                function cliMove(direction) {
+                    $('.img-list').stop(false, true);
+                    if (direction == "right") {
+                        index++;
+                        if (index > 5) {
+                            $('.img-list').css({ left: '0' })
+                            index = 1;
+                        }
+                    } else if (direction == "left") {
+                        index--;
+                        if (index < 0) {
+                            $('.img-list').css({
+                                left: maxNum * -limWidth
+                            })
+                            index = 4;
+                        }
+                    }
+                    $('.img-list').animate({
+                        left: index * -limWidth
+                    });
+                }
+                $('.title').mouseenter(function() {
+                    clearInterval(counter);
+                    counter = null;
+                }).mouseleave(function() {
+                    autoMove();
+                })
+                $('.right').click(function() {
+                    cliMove('right');
+                })
+                $('.left').click(function() {
+                    cliMove('left');
+                })
                 autoMove();
-            })
-            $('.right').click(function() {
-                cliMove('right');
-            })
-            $('.left').click(function() {
-                cliMove('left');
-            })
-            $('.item').click(function() {
-                index = $(this).index();
-                cliMove('');
+
             })
 
-            function dotMove(dom) {
-                $('.active').removeClass('active');
-                dom.addClass('active');
-            }
+            // 标题轮播图 带圆点
+            $(function() {
+                //轮播图2
+                var minNum = 0;
+                var maxNum = 5;
+                // 刚开始索引为0
+                var index = 0;
+                // 设置一个计时器为空
+                var counter = null;
+                var limWidth = 1280;
+                // 设置自动轮播函数
+                function autoMove() {
+                    if (counter != null) {
+                        return;
+                    }
+                    counter = setInterval(autoPlay, 3000);
+                }
 
-            autoMove();
+                function autoPlay() {
+                    cliMove('right');
+                }
+
+                function cliMove(direction) {
+                    $('.img-list1').stop(false, true);
+                    if (direction == "right") {
+                        index++;
+                        if (index > 5) {
+                            $('.img-list1').css({ left: '0' })
+                            index = 1;
+                        }
+                    } else if (direction == "left") {
+                        index--;
+                        if (index < 0) {
+                            $('.img-list1').css({
+                                left: maxNum * -limWidth
+                            })
+                            index = 4;
+                        }
+                    }
+                    dotMove($('.item').eq(index > 4 ? 0 : index));
+                    $('.img-list1').animate({
+                        left: index * -limWidth
+                    });
+                }
+                $('.bannar1').mouseenter(function() {
+                    clearInterval(counter);
+                    counter = null;
+                }).mouseleave(function() {
+                    autoMove();
+                })
+                $('.right1').click(function() {
+                    cliMove('right');
+                })
+                $('.left1').click(function() {
+                    cliMove('left');
+                })
+                $('.item').click(function() {
+                    index = $(this).index();
+                    cliMove('');
+                })
+                $('.right1').mouseenter(function() {
+                    clearInterval(counter);
+                    counter = null;
+                }).mouseleave(function() {
+                    autoMove();
+                })
+                $('.left1').mouseenter(function() {
+                    clearInterval(counter);
+                    counter = null;
+                }).mouseleave(function() {
+                    autoMove();
+                })
+                $('.dot-list').mouseenter(function() {
+                    clearInterval(counter);
+                    counter = null;
+                }).mouseleave(function() {
+                    autoMove();
+                })
+
+                function dotMove(dom) {
+                    $('.active').removeClass('active');
+                    dom.addClass('active');
+                }
+                autoMove();
+
+            })
+
+
+
+
         }
     }
 });
